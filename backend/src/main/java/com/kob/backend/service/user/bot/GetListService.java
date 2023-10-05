3b2ed65f85463f4ab73c4d7e85f6ccd55b1a25d3
lrs lrs <2553348955@qq.com>
2023-10-05 09:45:30 +0800

    创建个人中心页面上

---
+++ backend/src/main/java/com/kob/backend/service/user/bot/GetListService.java
@@ -0,0 +1,9 @@
+package com.kob.backend.service.user.bot;
+
+import com.kob.backend.pojo.Bot;
+
+import java.util.List;
+
+public interface GetListService {
+    List<Bot>getList();//蛇存在token里面可以不用传参数
+}
