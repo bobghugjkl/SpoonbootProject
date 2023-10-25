10b110fe17824e5508447105a68977b760019def
lrs lrs <2553348955@qq.com>
2023-10-25 22:11:03 +0800

    实现微服务：匹配系统（下）

---
+++ backendcloud/backend/src/main/java/com/kob/backend/pojo/Bot.java
@@ -0,0 +1,27 @@
+package com.kob.backend.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Bot {
+    @TableId(type = IdType.AUTO)//主键自增添加标志
+    private Integer id;
+    private Integer userId;//数据库里用下划线,这里就用驼峰命名法
+    private String title;
+    private String description;
+    private String content;
+    //private Integer rating;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "Asia/Shanghai")//日期展示格式
+    private Date createtime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "Asia/Shanghai")//日期展示格式
+    private Date modifytime;
+}
